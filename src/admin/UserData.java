936be8fd975050df7f821dbe196291c23c31b7b5
martin-d-wojcik <martin-d-wojcik@gmail.com>
2021-03-23 13:47:25 +0100

    created new class UserData

--- src/admin/UserData.java
+++ src/admin/UserData.java
@@ -1,4 +1,39 @@
 package admin;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class UserData {
+    private final StringProperty userName;
+    private final StringProperty userPassword;
+
+    public UserData(String name, String password) {
+        this.userName = new SimpleStringProperty(name);
+        this.userPassword = new SimpleStringProperty(password);
+
+    }
+
+    public String getUserName() {
+        return userName.get();
+    }
+
+    public StringProperty userNameProperty() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName.set(userName);
+    }
+
+    public String getUserPassword() {
+        return userPassword.get();
+    }
+
+    public StringProperty userPasswordProperty() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword.set(userPassword);
+    }
 }
