c68a497412bc1f6e9eefed46f75b2906b1d2152e
martin-d-wojcik <martin-d-wojcik@gmail.com>
2021-04-09 22:00:22 +0200

    added new class PatientsController

--- src/patients/PatientsController.java
+++ src/patients/PatientsController.java
@@ -1,2 +1,22 @@
-package patients;public class PatientsController {
+package patients;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PatientsController implements Initializable {
+    @FXML
+    private Label lblUserLoggedInHeader;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {}
+
+    public PatientsController() {}
+
+    public void setHeader(String userName) {
+        lblUserLoggedInHeader.setText(userName);
+    }
 }
