e3c44b37bbb8614e0bda80c1e828b83dc7b62936
martin-d-wojcik <martin-d-wojcik@gmail.com>
2021-04-13 22:34:16 +0200

    added displaying all patients in a tableview in patients fxml

--- src/patients/PatientData.java
+++ src/patients/PatientData.java
@@ -1,2 +1,124 @@
-package patients;public class PatientData {
+package patients;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class PatientData {
+    private final StringProperty patientPersonNr;
+    private final StringProperty patientFirstName;
+    private final StringProperty patientLastName;
+    private final StringProperty patientStreetAdress;
+    private final StringProperty patientCity;
+    private final StringProperty patientPostalCode;
+    private final StringProperty patientEmail;
+    private final StringProperty patientPhoneNr;
+
+    public PatientData(String patientPersonNr, String patientFirstName, String patientLastName,
+                       String patientStreetAdress, String patientCity, String patientPostalCode,
+                       String patientEmail, String patientPhoneNr) {
+        this.patientPersonNr = new SimpleStringProperty(patientPersonNr);
+        this.patientFirstName = new SimpleStringProperty(patientFirstName);
+        this.patientLastName = new SimpleStringProperty(patientLastName);
+        this.patientStreetAdress = new SimpleStringProperty(patientStreetAdress);
+        this.patientCity = new SimpleStringProperty(patientCity);
+        this.patientPostalCode = new SimpleStringProperty(patientPostalCode);
+        this.patientEmail = new SimpleStringProperty(patientEmail);
+        this.patientPhoneNr = new SimpleStringProperty(patientPhoneNr);
+    }
+
+    public String getPatientPersonNr() {
+        return patientPersonNr.get();
+    }
+
+    public StringProperty patientPersonNrProperty() {
+        return patientPersonNr;
+    }
+
+    public void setPatientPersonNr(String patientPersonNr) {
+        this.patientPersonNr.set(patientPersonNr);
+    }
+
+    public String getPatientFirstName() {
+        return patientFirstName.get();
+    }
+
+    public StringProperty patientFirstNameProperty() {
+        return patientFirstName;
+    }
+
+    public void setPatientFirstName(String patientFirstName) {
+        this.patientFirstName.set(patientFirstName);
+    }
+
+    public String getPatientLastName() {
+        return patientLastName.get();
+    }
+
+    public StringProperty patientLastNameProperty() {
+        return patientLastName;
+    }
+
+    public void setPatientLastName(String patientLastName) {
+        this.patientLastName.set(patientLastName);
+    }
+
+    public String getPatientStreetAdress() {
+        return patientStreetAdress.get();
+    }
+
+    public StringProperty patientStreetAdressProperty() {
+        return patientStreetAdress;
+    }
+
+    public void setPatientStreetAdress(String patientStreetAdress) {
+        this.patientStreetAdress.set(patientStreetAdress);
+    }
+
+    public String getPatientCity() {
+        return patientCity.get();
+    }
+
+    public StringProperty patientCityProperty() {
+        return patientCity;
+    }
+
+    public void setPatientCity(String patientCity) {
+        this.patientCity.set(patientCity);
+    }
+
+    public String getPatientPostalCode() {
+        return patientPostalCode.get();
+    }
+
+    public StringProperty patientPostalCodeProperty() {
+        return patientPostalCode;
+    }
+
+    public void setPatientPostalCode(String patientPostalCode) {
+        this.patientPostalCode.set(patientPostalCode);
+    }
+
+    public String getPatientEmail() {
+        return patientEmail.get();
+    }
+
+    public StringProperty patientEmailProperty() {
+        return patientEmail;
+    }
+
+    public void setPatientEmail(String patientEmail) {
+        this.patientEmail.set(patientEmail);
+    }
+
+    public String getPatientPhoneNr() {
+        return patientPhoneNr.get();
+    }
+
+    public StringProperty patientPhoneNrProperty() {
+        return patientPhoneNr;
+    }
+
+    public void setPatientPhoneNr(String patientPhoneNr) {
+        this.patientPhoneNr.set(patientPhoneNr);
+    }
 }
--- src/patients/PatientsController.java
+++ src/patients/PatientsController.java
@@ -1,14 +1,23 @@
 package patients;
 
+import dbUtil.dbConnection;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.shape.Line;
-import resources.Styling;
+import resources.StylingLeftMenu;
 
-import java.awt.event.ActionEvent;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class PatientsController implements Initializable {
@@ -19,42 +28,112 @@ public class PatientsController implements Initializable {
     private AnchorPane anchorPaneLeftmenu;
 
     @FXML
-    private Label lblPatienter;
+    private Button btnPatients;
 
     @FXML
-    private Label lblJournaler;
+    private Button btnJournaler;
 
     @FXML
-    private Label lblProgram;
+    private Button btnProgram;
 
     @FXML
-    private Label lblHem;
+    private Button btnStart;
 
     @FXML
-    private Line lineLeftMenuTop;
+    private Button btnAllPatients;
 
     @FXML
-    private Label lblTemp;
+    private TableView<PatientData> tableViewPatients;
+
+    @FXML
+    private TableColumn<PatientData, String> tableColumnPersonNr;
+
+    @FXML
+    private TableColumn<PatientData, String> tableColumnFirstName;
+
+    @FXML
+    private TableColumn<PatientData, String> tableColumnLastName;
+
+    @FXML
+    private TableColumn<PatientData, String> tableColumnStreetAdress;
+
+    @FXML
+    private TableColumn<PatientData, String> tableColumnCity;
+
+    @FXML
+    private TableColumn<PatientData, String> tableColumnPostalCode;
+
+    @FXML
+    private TableColumn<PatientData, String> tableColumnEmail;
+
+    @FXML
+    private TableColumn<PatientData, String> tableColumnPhoneNr;
+
+    private String sqlGetAllCustomersBasic = "SELECT personNr, firstName, lastName, streetAdress, city, postalCode, email, phoneNumber FROM customer";
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        // styling
-        lblUserLoggedInHeader.setStyle("-fx-text-fill: " + Styling.loggedInUserHeaderTextFill);
-        anchorPaneLeftmenu.setStyle("-fx-background-color: " + Styling.backgroundDarkGrey);
-        lblHem.setStyle("-fx-text-fill: " + Styling.itemSelectedInLeftMenuTextFill);
-        lblPatienter.setStyle("-fx-text-fill: " + Styling.loggedInUserHeaderTextFill);
-        lblJournaler.setStyle("-fx-text-fill: " + Styling.loggedInUserHeaderTextFill);
-        lblProgram.setStyle("-fx-text-fill: " + Styling.loggedInUserHeaderTextFill);
-        // lineLeftMenuTop.setStyle("-fx-text-fill: " + Styling.loggedInUserHeaderLine);
+        // styling left menu
+        anchorPaneLeftmenu.setStyle("-fx-background-color: " + StylingLeftMenu.BACKGROUND_DARK_GREY);
+        lblUserLoggedInHeader.setStyle("-fx-text-fill: " + StylingLeftMenu.LOGGED_IN_USER_HEADER_TEXT_FILL);
+
+        // styling left menu - buttons
+        btnPatients.setStyle("-fx-background-color: " + StylingLeftMenu.BACKGROUND_DARK_GREY
+                + "; -fx-text-fill: " + StylingLeftMenu.ITEM_SELECTED_IN_LEFT_MENU_TEXT_FILL);
+        btnJournaler.setStyle("-fx-background-color: " + StylingLeftMenu.BACKGROUND_DARK_GREY
+                + "; -fx-text-fill: " + StylingLeftMenu.ITEMS_IN_LEFT_MENU_TEXT_FILL);
+        btnProgram.setStyle("-fx-background-color: " + StylingLeftMenu.BACKGROUND_DARK_GREY
+                + "; -fx-text-fill: " + StylingLeftMenu.ITEMS_IN_LEFT_MENU_TEXT_FILL);
+        btnStart.setStyle("-fx-background-color: " + StylingLeftMenu.BACKGROUND_DARK_GREY +
+                "; -fx-text-fill: " + StylingLeftMenu.ITEMS_IN_LEFT_MENU_TEXT_FILL);
+        btnPatients.setAlignment(Pos.BASELINE_LEFT);
+        btnProgram.setAlignment(Pos.BASELINE_LEFT);
+        btnJournaler.setAlignment(Pos.BASELINE_LEFT);
+        btnStart.setAlignment(Pos.BASELINE_LEFT);
 
         setHeader(login.UserHolder.getLoggedInUser());
+        fillTableView();
     }
 
     public void setHeader(String userName) {
         lblUserLoggedInHeader.setText("Inloggad: " + userName);
     }
 
-    public void goToPatients(ActionEvent event) {
-        lblTemp.setText("Boom");
+    private void fillTableView() {
+        ObservableList<PatientData> data = FXCollections.observableArrayList();
+
+        try {
+            Connection conn = dbConnection.getConnection();
+            assert conn != null;
+            ResultSet rs = conn.createStatement().executeQuery(sqlGetAllCustomersBasic);
+
+            // check if the resultSet, rs has anything in the table
+            while (rs.next()) {
+                data.add(new PatientData(rs.getString(1), rs.getString(2), rs.getString(3),
+                        rs.getString(4), rs.getString(5), rs.getString(6),
+                        rs.getString(7), rs.getString(8)));
+            }
+            conn.close();
+
+        } catch (SQLException e) {
+            System.err.println("Error: " + e);
+        }
+
+        // get the StringProperties from the UserData class
+        this.tableColumnPersonNr.setCellValueFactory(new PropertyValueFactory<PatientData, String>("patientPersonNr"));
+        this.tableColumnFirstName.setCellValueFactory(new PropertyValueFactory<PatientData, String>("patientFirstName"));
+        this.tableColumnLastName.setCellValueFactory(new PropertyValueFactory<PatientData, String>("patientLastName"));
+        this.tableColumnStreetAdress.setCellValueFactory(new PropertyValueFactory<PatientData, String>("patientStreetAdress"));
+        this.tableColumnCity.setCellValueFactory(new PropertyValueFactory<PatientData, String>("patientCity"));
+        this.tableColumnPostalCode.setCellValueFactory(new PropertyValueFactory<PatientData, String>("patientPostalCode"));
+        this.tableColumnEmail.setCellValueFactory(new PropertyValueFactory<PatientData, String>("patientEmail"));
+        this.tableColumnPhoneNr.setCellValueFactory(new PropertyValueFactory<PatientData, String>("patientPhoneNr"));
+
+        this.tableViewPatients.setItems(null);
+        this.tableViewPatients.setItems(data);
+    }
+
+    public void ListAllPatients(javafx.event.ActionEvent event) {
+        fillTableView();
     }
 }
