4d686ab32b14fea2d3d449ccdd5a74d71b8e76c1
martin-d-wojcik <martin-d-wojcik@gmail.com>
2021-05-08 22:51:54 +0200

    added populate exercise table view in exercise fxml

---
+++ src/exercises/ExerciseData.java
@@ -0,0 +1,66 @@
+package exercises;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class ExerciseData {
+    private final StringProperty exerciseName;
+    private final StringProperty type;
+    private final StringProperty bodyPart;
+    private final StringProperty description;
+
+    public ExerciseData(String exerciseName, String type, String bodyPart, String description) {
+        this.exerciseName = new SimpleStringProperty(exerciseName);
+        this.type = new SimpleStringProperty(type);
+        this.bodyPart = new SimpleStringProperty(bodyPart);
+        this.description = new SimpleStringProperty(description);
+    }
+
+    public String getExerciseName() {
+        return exerciseName.get();
+    }
+
+    public StringProperty exerciseNameProperty() {
+        return exerciseName;
+    }
+
+    public void setExerciseName(String exerciseName) {
+        this.exerciseName.set(exerciseName);
+    }
+
+    public String getType() {
+        return type.get();
+    }
+
+    public StringProperty typeProperty() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type.set(type);
+    }
+
+    public String getBodyPart() {
+        return bodyPart.get();
+    }
+
+    public StringProperty bodyPartProperty() {
+        return bodyPart;
+    }
+
+    public void setBodyPart(String bodyPart) {
+        this.bodyPart.set(bodyPart);
+    }
+
+    public String getDescription() {
+        return description.get();
+    }
+
+    public StringProperty descriptionProperty() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description.set(description);
+    }
+}
--- src/exercises/ExercisesController.java
+++ src/exercises/ExercisesController.java
@@ -6,14 +6,12 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import resources.StylingLayout;
 import java.net.URL;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -51,9 +49,22 @@ public class ExercisesController implements Initializable {
     @FXML
     private Button btnNewExercise;
 
+    // table
+    @FXML
+    private TableView<ExerciseData> tableViewExercises;
+    @FXML
+    private TableColumn<ExerciseData, String> tableColumnName;
+    @FXML
+    private TableColumn<ExerciseData, String> tableColumnBodyPart;
+    @FXML
+    private TableColumn<ExerciseData, String> tableColumntype;
+    @FXML
+    private TableColumn<ExerciseData, String> tableColumnDescription;
+
     // sql queries
     private String sqlQueryType = "SELECT DISTINCT type FROM exercise";
     private String sqlQueryBodyPart = "SELECT DISTINCT bodyPart FROM exercise";
+    private String sqlQueryAllExercises = "SELECT exerciseName, bodyPart, type, description FROM exercise";
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -76,6 +87,8 @@ public class ExercisesController implements Initializable {
 
         comboBoxType.setItems(FXCollections.observableArrayList(getTypeData()));
         comboBoxBodyPart.setItems(FXCollections.observableArrayList(getBodyPartData()));
+
+        fillTableView();
     }
 
     private List<String> getBodyPartData() {
@@ -120,6 +133,38 @@ public class ExercisesController implements Initializable {
             ex.printStackTrace();
             return null;
         }
+    }
+
+    private void fillTableView() {
+        ObservableList<ExerciseData> data = FXCollections.observableArrayList();
+
+        try {
+            Connection conn = dbConnection.getConnection();
+            assert conn != null;
+            ResultSet rs = conn.createStatement().executeQuery(sqlQueryAllExercises);
+
+            // check if the resultSet, rs has anything in the table
+            while (rs.next()) {
+                data.add(new ExerciseData(rs.getString(1), rs.getString(2),
+                        rs.getString(3), rs.getString(4)));
+            }
+            conn.close();
+
+        } catch (SQLException e) {
+            System.err.println("Error: " + e);
+        }
+
+        fillTableWithExerciseData(data);
+    }
+
+    private void fillTableWithExerciseData(ObservableList<ExerciseData> data) {
+        this.tableColumnName.setCellValueFactory(new PropertyValueFactory<ExerciseData, String>("exerciseName"));
+        this.tableColumnBodyPart.setCellValueFactory(new PropertyValueFactory<ExerciseData, String>("bodyPart"));
+        this.tableColumntype.setCellValueFactory(new PropertyValueFactory<ExerciseData, String>("type"));
+        this.tableColumnDescription.setCellValueFactory(new PropertyValueFactory<ExerciseData, String>("description"));
+
+        this.tableViewExercises.setItems(null);
+        this.tableViewExercises.setItems(data);
     }
 
     public void GoToPatients(javafx.event.ActionEvent event) {
